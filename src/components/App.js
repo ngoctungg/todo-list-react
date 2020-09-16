274bd2e160dd7bc413afff8efe8f0824fddf7de8
ngoctungg <m.ngoctung@gmail.com>
2020-09-16 21:25:01 +0700

    re-structure file

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../res/logo.svg';
+import '../css/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p >
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="button is-primary"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
